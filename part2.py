03fe3f799dde50e8ed18404a79cc0350a41871ab
Ortal Yatzkan <ortalyatzkan@gmail.com>
2022-08-04 13:28:56 +0300

    add crop function

--- part2.py
+++ part2.py
@@ -8,8 +8,7 @@ try:
     import glob
     import argparse
     import h5py
-
-
+    from PIL import Image
     import numpy as np
 
 
@@ -24,7 +23,25 @@ except ImportError:
     raise
 
 
+
+
+def crop_image(img_path: str, x: int, y: int, zoom: int):
+    im = Image.open("r"+img_path)
+    width, height = im.size
+    # Setting the points for cropped image
+    # Cropped image of above dimension
+    # (It will not change original image)
+    #im1 = im.crop((left, top, right, bottom))
+
+    im1 = im.crop(x-15, y+30, x+15, y-30)
+
+    # Shows the image in image viewer
+    im1.show()
+
+
 if __name__ == '__main__':
     df = pd.read_hdf('attention_results.h5')
     pd.set_option('display.max_rows', None)
     print(df)
+
+    crop_image()
