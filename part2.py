0da550a59d18e4a3bd59367c4ecf45cb6d066fa0
Ortal Yatzkan <ortalyatzkan@gmail.com>
2022-08-05 17:14:25 +0300

    Fix crop_image function

--- part2.py
+++ part2.py
@@ -25,29 +25,31 @@ SRC_PATH = "leftImg8bit/train/aachen"
 def get_table() -> object:
     save_table = pd.read_hdf('attention_results.h5')
     pd.set_option('display.max_rows', None)
-    return object
+    print(save_table)
+    return save_table
 
 
 def crop_images_from_table():
+    counter=0
     for index, row in df.iterrows():
-        if str(row["path"]).startswith("bochum"):
-            crop_image(row['path'], int(row['x']), int(row['y']), row['zoom'])
+        if str(row["path"]):
+            crop_image(row['path'], int(row['x']), int(row['y']), row['zoom'],counter)
+            counter+=1
 
 
-def crop_image(img_path: str, x: int, y: int, zoom: int):
+def crop_image(img_path: str, x: int, y: int, zoom: int, index: int):
     im = Image.open(SRC_PATH+'/'+img_path)
+    label_name = img_path.replace("leftImg8bit", "gtFine_color")
+
+    im_label = Image.open(FILTER_PATH+'/'+label_name)
     width, height = im.size
-    # Setting the points for cropped image
-    # Cropped image of above dimension
-    # (It will not change original image)
-    #im1 = im.crop((left, top, right, bottom))
-
-    im1 = im.crop((x-15, y-30, x+15, y+30))
-
-    # Shows the image in image viewer
-
-    plt.imshow(im1)  # print image with opacity filter
-    plt.show()
+    im_of_label = im_label.crop((x-15, y-30, x+15, y+30))
+    pixel_arr = np.array(im_of_label)
+    save_orange_pixel = np.where(pixel_arr == [250, 170, 30, 255])
+    print(len(save_orange_pixel[0]))
+    if len(save_orange_pixel[0]) >4000:
+        im.crop((x - 15, y - 30, x + 15, y + 30)).save("crop_images/crop" + str(index) + ".png", format="png")
+        im_of_label.save("crop_images/crop" + str(index) + "label.png", format="png")
 
 
 if __name__ == '__main__':
